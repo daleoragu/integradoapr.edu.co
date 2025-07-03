266de690ee2119428255382aabcd9d4b4761bf2a
David Leonardo Ramos Gutierrez <davidlrg89@gmail.com>
2025-07-03 10:04:33 -0500

    Agrego storage personalizado para archivos públicos

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,8 @@ AWS_S3_OBJECT_PARAMETERS = {
     'ACL': 'public-read',
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'notas.storage_backends.PublicMediaStorage'
+
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
---
+++ notas/storage_backends.py
@@ -0,0 +1,10 @@
+# notas/storage_backends.py
+
+from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = ''
+    default_acl = 'public-read'
+    file_overwrite = False
+    custom_domain = f"{settings.AWS_STORAGE_BUCKET_NAME}.s3.{settings.AWS_S3_REGION_NAME}.amazonaws.com"
