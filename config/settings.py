03d6c2f3d83aaeff2af66d82b3020bb0d6829ee4
David Leonardo Ramos Gutierrez <davidlrg89@gmail.com>
2025-07-04 15:35:46 -0500

    Finaliza configuración para despliegue en Render y desarrollo local

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1 ','integradoapr.edu.co','www.integradoapr.edu.co').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
